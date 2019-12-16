b6345e134672e6aa1c1775aff1cdf1bb14d9d59f
KIM HONGIL <clroyan01@gmail.com>
2019-12-17 00:42:50 +0900

    Create domain - hero

---
+++ src/main/java/api/documentation/swagger/domain/hero/Hero.java
@@ -0,0 +1,35 @@
+package api.documentation.swagger.domain.hero;
+
+import api.documentation.swagger.domain.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Hero extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int age;
+
+    private String note;
+
+    @Builder
+    public Hero(String name, int age, String note) {
+        this.name = name;
+        this.age = age;
+        this.note = note;
+    }
+}
