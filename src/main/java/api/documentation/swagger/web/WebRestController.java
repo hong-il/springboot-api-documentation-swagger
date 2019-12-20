c1fd3c4d76f53f0200fb015bf761aafcaf0a4a8d
KIM HONGIL <clroyan01@gmail.com>
2019-12-20 14:46:11 +0900

    Create rest api - create

--- src/main/java/api/documentation/swagger/web/WebRestController.java
+++ src/main/java/api/documentation/swagger/web/WebRestController.java
@@ -1,7 +1,9 @@
 package api.documentation.swagger.web;
 
+import api.documentation.swagger.domain.hero.Hero;
 import api.documentation.swagger.domain.hero.HeroRepository;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,4 +14,15 @@ public class WebRestController {
 
     private HeroRepository heroRepository;
 
+    @ApiOperation(value = "save", notes = "github.com/hong-il")
+    @PostMapping("/save")
+    public Hero HeroSaveRequest(@RequestParam String name,
+                                @RequestParam int age,
+                                @RequestParam String note) {
+        return heroRepository.save(Hero.builder()
+                .name(name)
+                .age(age)
+                .note(note)
+                .build());
+    }
 }
